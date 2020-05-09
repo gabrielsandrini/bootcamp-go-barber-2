0f2b0eb622c862c00fc37a06bae550aecfe78806
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-05-09 17:07:23 -0300

    Relacionamentos nos models

--- src/database/migrations/1589039090414-CreateUsers.ts
+++ src/database/migrations/1589039090414-CreateUsers.ts
@@ -8,7 +8,7 @@ export class CreateUsers1589039090414 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
