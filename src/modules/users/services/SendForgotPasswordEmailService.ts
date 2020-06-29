b0349ab25318b02c6081fa2fd3eaa32fd90bef87
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-06-29 03:56:11 -0300

    Nodemailer with Ethereal

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -29,9 +29,12 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists.');
     }
 
-    this.userTokensRepository.generate(user.id);
+    const { token } = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha');
+    await this.mailProvider.sendMail(
+      email,
+      `Pedido de recuperação de senha ${token}`,
+    );
   }
 }
 
