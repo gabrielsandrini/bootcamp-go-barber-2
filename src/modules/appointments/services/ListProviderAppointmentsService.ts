9a3b0febcff5e366b2ed3d774e3c245c8699f500
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-07-14 22:26:29 -0300

    Cache with redis

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -14,6 +15,9 @@ class ListProvidersAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -22,6 +26,9 @@ class ListProvidersAppointmentsService {
     month,
     year,
   }: IRequest): Promise<Appointment[]> {
+    // const cachedData = await this.cacheProvider.recover('asd');
+    // console.log(cachedData);
+
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
       {
         provider_id,
@@ -30,6 +37,8 @@ class ListProvidersAppointmentsService {
         year,
       },
     );
+
+    // await this.cacheProvider.save('asd', 'asd2');
 
     return appointments;
   }
--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 import IUserRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   user_id?: string;
@@ -10,14 +11,25 @@ class UpdateUserProfileService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUserRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const user = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
 
-    return user;
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+    }
+
+    this.cacheProvider.save(`providers-list:${user_id}`, users);
+
+    return users;
   }
 }
 export default UpdateUserProfileService;
