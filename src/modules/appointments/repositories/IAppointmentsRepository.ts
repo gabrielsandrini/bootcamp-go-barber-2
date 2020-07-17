c95adfcb9eef0a668034b637a26b2ac4f6144964
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-07-16 22:01:41 -0300

    findByDate in Appointments searching by date and provider_id

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,12 +1,12 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 
-import ICreateappointmentDTO from '../dtos/ICreateAppointmentDTO';
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
-  create(data: ICreateappointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
+  findByDate(date: Date, provider_id: string): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProviderDTO,
   ): Promise<Appointment[]>;
