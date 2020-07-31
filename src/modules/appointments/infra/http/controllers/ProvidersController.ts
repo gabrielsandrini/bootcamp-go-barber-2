281db8838aea78c37b9f32de990f62521eac3260
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-07-31 00:37:01 -0300

    class transform

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,5 +1,6 @@
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
+import { classToClass } from 'class-transformer';
 
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
@@ -11,7 +12,7 @@ class ProvidersController {
 
     const providers = await listProvidersService.execute({ user_id });
 
-    return response.json(providers);
+    return response.json(classToClass(providers));
   }
 }
 
